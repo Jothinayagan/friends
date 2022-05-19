da97c65fa885248637c6140096801dcebbcbc396
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-05-19 11:24:32 +0530

    Add username field & whitelist username for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_115810) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_041929) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_115810) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
